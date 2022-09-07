531d6be5ab4ff00f466ebeae13acac4c60083aab
Стив <kenethra.74@gmail.com>
2022-09-07 17:55:03 -0600

    feat(frontend): register page

--- lireddit-web/src/components/InputField.tsx
+++ lireddit-web/src/components/InputField.tsx
@@ -8,13 +8,13 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({label, size ,...props}) => {
   const [field, {error}] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder}/>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name}/>
       { error ? <FormErrorMessage>{error}</FormErrorMessage> : null }
     </FormControl>
   )
